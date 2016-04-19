bb338738d11ac775a8d6660a31893a3674613c7d
Antonio Salvati <antonio.salvati@skillbill.it>
2016-04-19 15:19:03 +0200

    damn prefixes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ window.addEventListener('load', function() {
   var deg = 0;
   var update = function() {
     deg += 10;
-    main.style.filter = "hue-rotate(" + deg + "deg)";
+    if(CSS.supports("filter", "hue-rotate(1deg)")) {
+      main.style.filter = "hue-rotate(" + deg + "deg)";
+    } else if(CSS.supports("-webkit-filter", "hue-rotate(1deg)")) {
+      main.style.webkitFilter = "hue-rotate(" + deg + "deg)";
+    }
   };
   update();
   setInterval(update, 200);
